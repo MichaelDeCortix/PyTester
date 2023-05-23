f6d1c2734362bc9e65559ec2f93aef00cf5f1c6d
Michael <mideco.tech@gmail.com>
2023-05-23 16:01:26 +0300

    Fixed conflicting directories

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .db.postgres import postgre_exec
-from .db.oracle import oracle_exec
-from .http.requests_handler import request
+from .db_module.postgres import postgre_exec
+from .db_module.oracle import oracle_exec
+from .http_module.requests_handler import request
 from .widgets.display_and_copy import display_and_copy
 
 __version__ = "0.1.0"
