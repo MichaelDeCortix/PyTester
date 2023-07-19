fde427b7bb98a703d30f58465cedb8ae3edf0a9c
Michael <mideco.tech@gmail.com>
2023-07-19 20:22:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytester",
-    version="0.0.1",
+    version="0.0.2",
     url="https://github.com/MichaelDeCortix/pyTester",
     author="MichaelDeCortix",
     description="Description of my package",
